98d3c5759ebecd3208cf0b625aaf9fd270f48e2a
Mickaël Wolff <mickael@lupusmic.org>
2019-02-01 11:13:23 +0100

    Tell fate in rectangular shapes

--- tests/GridRunTests.elm
+++ tests/GridRunTests.elm
@@ -1,5 +1,6 @@
 module GridRunTests exposing
     ( cellForetoldFateTests
+    , cellInRectangularSandbox
     )
 
 import Grid
@@ -119,6 +120,123 @@ cellForetoldFateTests =
                             , e, e, e, d, l, e
                             , e, e, e, l, l, e
                             , e, e, e, e, e, e
+                            ]
+                            |> Grid.makeFromList
+                                (Grid.makeDimension 5 5)
+
+                        target:  Maybe Grid.Grid
+                        target =
+                            origin
+
+                    in
+                        Maybe.map Grid.run origin
+                            |> Expect.all
+                                [ Expect.equal intermediate
+                                , (\asset -> Maybe.map Grid.run asset
+                                    |> Expect.equal target) -- this lambda's quite retard, is there a better syntax?
+                                ]
+                )
+            ]
+
+cellInRectangularSandbox: Test
+cellInRectangularSandbox =
+    let
+        l = Grid.Cell.Live
+        d = Grid.Cell.Deceased
+        e = Grid.Cell.Empty
+    in
+        describe
+            """
+            See how patterns are evolving in non-squared sandboxes
+            """
+            [ test "Still life: block"
+                (\_ ->
+                    let
+                        block: List Grid.Cell.State
+                        block =
+                            [ e, e, e, e, e, e, e
+                            , e, l, l, e, e, e, e
+                            , e, l, l, e, e, e, e
+                            , e, e, e, e, e, e, e
+                            ]
+
+                        grid: Maybe Grid.Grid
+                        grid =
+                            Grid.makeFromList
+                                (Grid.makeDimension 4 4)
+                                block
+
+                    in
+                        Maybe.map Grid.run grid
+                            |> Expect.equal grid
+                )
+            , test "Oscillator: blinker"
+                (\_ ->
+                    let
+                        block: List Grid.Cell.State
+                        block =
+                            [ e, e, e, e, e
+                            , e, l, l, l, e
+                            , e, e, e, e, e
+                            ]
+
+                        origin: Maybe Grid.Grid
+                        origin =
+                            Grid.makeFromList
+                                (Grid.makeDimension 5 5)
+                                block
+
+                        intermediate:  Maybe Grid.Grid
+                        intermediate =
+                            [ e, e, l, e, e
+                            , e, d, l, d, e
+                            , e, e, l, e, e
+                            ]
+                            |> Grid.makeFromList
+                                (Grid.makeDimension 5 5)
+
+                        target:  Maybe Grid.Grid
+                        target =
+                            [ e, e, d, e, e
+                            , e, l, l, l, e
+                            , e, e, d, e, e
+                            ]
+                            |> Grid.makeFromList
+                                (Grid.makeDimension 5 5)
+
+                    in
+                        Maybe.map Grid.run origin
+                            |> Expect.all
+                                [ Expect.equal intermediate
+                                , (\asset -> Maybe.map Grid.run asset
+                                    |> Expect.equal target) -- this lambda's quite retard, is there a better syntax?
+                                ]
+                )
+            , test "Oscillator: beacon"
+                (\_ ->
+                    let
+                        block: List Grid.Cell.State
+                        block =
+                            [ e, e, e, e
+                            , l, l, e, e
+                            , l, l, e, e
+                            , e, e, l, l
+                            , e, e, l, l
+                            ]
+
+                        origin: Maybe Grid.Grid
+                        origin =
+                            Grid.makeFromList
+                                (Grid.makeDimension 5 5)
+                                block
+
+                        intermediate:  Maybe Grid.Grid
+                        intermediate =
+                            [ e, e, e, e
+                            , l, l, e, e
+                            , l, d, e, e
+                            , e, e, d, l
+                            , e, e, l, l
                             ]
                             |> Grid.makeFromList
                                 (Grid.makeDimension 5 5)
