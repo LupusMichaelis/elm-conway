343759a2f06bda204df24de87ee623e8cd01a7d0
Mickaël Wolff <mickael@lupusmic.org>
2019-04-22 02:24:18 +0200

    Basic helper removed in Elm 0.19

--- src/Basic.elm
+++ src/Basic.elm
@@ -1,6 +1,26 @@
 module Basic exposing
-    ( flip
+    ( curry
+    , flip
+    , swap
+    , uncurry
     )
 
+
 flip : (b -> a -> c) -> a -> b -> c
-flip f a b = f b a
+flip f a b =
+    f b a
+
+
+curry : (( a, b ) -> c) -> a -> b -> c
+curry f a b =
+    f ( a, b )
+
+
+uncurry : (a -> b -> c) -> ( a, b ) -> c
+uncurry f ( a, b ) =
+    f a b
+
+
+swap : ( a, b ) -> ( b, a )
+swap ( a, b ) =
+    ( b, a )
